06abcb4f298df75493f8b2e9be8fe56cefca48bf
Daniil Moskovskiy <50806739+etomoscow@users.noreply.github.com>
2021-03-27 14:41:59 +0300

    fix

--- run_fusion.sh
+++ run_fusion.sh
@@ -1,53 +1,53 @@
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000.hdf5 \
-    -p abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000.hdf5 \
+    -p=validation/med_res/abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000.hdf5 \
-    -p abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000.hdf5 \
+    -p=validation/med_res/abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500149_54930d6f7740b03347d89a56_000.hdf5 \
-    -p abc_0050_00500149_54930d6f7740b03347d89a56_000__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500149_54930d6f7740b03347d89a56_000.hdf5 \
+    -p=validation/med_res/abc_0050_00500149_54930d6f7740b03347d89a56_000__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500166_5894bbd701b2bb0fc88a6978_007.hdf5 \
-    -p abc_0050_00500166_5894bbd701b2bb0fc88a6978_007__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500166_5894bbd701b2bb0fc88a6978_007.hdf5 \
+    -p=validation/med_res/abc_0050_00500166_5894bbd701b2bb0fc88a6978_007__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000.hdf5 \
-    -p abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000.hdf5 \
+    -p=validation/med_res/abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000.hdf5 \
-    -p abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000.hdf5 \
+    -p=validation/med_res/abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500683_511f7debb63f164003339dec_000.hdf5 \
-    -p abc_0050_00500683_511f7debb63f164003339dec_000__predictions.hdf5 \
-    -o results/med \
-    -r 0.05
+    -t=validation/med_res/abc_0050_00500683_511f7debb63f164003339dec_000.hdf5 \
+    -p=validation/med_res/abc_0050_00500683_511f7debb63f164003339dec_000__predictions.hdf5 \
+    -o=results/med \
+    -r=0.05
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500166_5894bbd701b2bb0fc88a6978_007.hdf5 \
-    -p abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000__predictions.hdf5 \
-    -o results/high \
-    -r 0.02
+    -t=validation/high_res/abc_0050_00500166_5894bbd701b2bb0fc88a6978_007.hdf5 \
+    -p=validation/high_res/abc_0050_00500082_4cb4bf14428fe3832dd7ed78_000__predictions.hdf5 \
+    -o=results/high \
+    -r=0.02
 
 python3 scripts/fuse_images.py \
-    -t abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000.hdf5 \
-    -p abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000__predictions.hdf5 \
-    -o results/high \
-    -r 0.02
+    -t=validation/high_res/abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000.hdf5 \
+    -p=validation/high_res/abc_0050_00500348_fae0ecd8b3dc068d39f0d09c_000__predictions.hdf5 \
+    -o=results/high \
+    -r=0.02
